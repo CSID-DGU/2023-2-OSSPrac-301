9b48b783cc155dede15131953e0429d38de2dbf6
NANGEUN <143536597+NANGEUN@users.noreply.github.com>
2023-10-30 16:48:12 +0900

    add gender check & programming language

--- ex3/prac7/ex3.py
+++ ex3/prac7/ex3.py
@@ -12,6 +12,9 @@ def result():
         result=dict()
         result['Name']=request.form.get('name')
         result['Student Number']=request.form.get('StudentNumber')
+        result['gender_check']=request.form.get('gender_check')
+        result['programming_language']=", ".join(request.form.getlist('programming_language'))
+
         return render_template('result.html',result=result)
 
 
